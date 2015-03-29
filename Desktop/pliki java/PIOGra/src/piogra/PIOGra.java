5bcd4fb4ef4de8dc2006ec31da549e574e7fe394
Michał <>
2015-03-29 14:14:41 +0200

    Zadanie 3 etap

--- Desktop/pliki java/PIOGra/src/piogra/PIOGra.java
+++ Desktop/pliki java/PIOGra/src/piogra/PIOGra.java
@@ -1,22 +1,29 @@
 
 package piogra;
 
+import piogra.players.PlayerHuman;
+import piogra.players.PlayerComp;
+import piogra.players.Player;
 import java.util.Random;
+
 
 public class PIOGra {
 
     public static void main(String[] args) { 
-        Player player = new Player();
+        
+        Player player = new PlayerComp();
         Random r = new Random();
+        player.setName("Janek");
+        player.setName(null);
         while(true){
             int randomInt1 = r.nextInt(6)+1;
-            int randomInt2 = player.guess();            
+            int randomInt2 = player.guess();             
             if(randomInt1 == randomInt2){
-                System.out.println("Gracz wygrał, wylosowana liczba to "+randomInt1);
-                break;
+                System.out.println(player.getName()+" wygrał, wylosowana liczba to "+randomInt1);
+                break; 
             } else {
-                System.out.println("Gracz wylosował zła liczbę, poprawna to :"
-                        +randomInt1+" a gracz wylosował :"+randomInt2);
+                System.out.println(player.getName() + " wylosował zła liczbę, poprawna to :"
+                        +randomInt1+" a "+player.getName()+" wylosował : "+randomInt2);
             }            
             
         }
